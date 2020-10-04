dc6ba97b2d4855d8b02be2b92a24e68e217aa60a
87stal <87stal@gmail.com>
2020-10-04 18:43:13 +0300

    Updates

---
+++ precache-manifest.3a48d3f541c613d89b621f83aaa68cfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b41ecf3227a8b75365395e89de1a6a11",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "a1379c5d58ced256600d",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.3341fed2.chunk.css"
+  },
+  {
+    "revision": "c43776ea073441c041c5",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.7a37c811.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.7a37c811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1379c5d58ced256600d",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.ca0b1f05.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
